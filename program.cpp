7bf7231bfbc5c6611f1f01e3794a61f851179ba8
Nagatyy <mohamednagaty@aucegypt.edu>
2020-10-12 22:41:01 +0200

    Commit6

--- program.cpp
+++ program.cpp
@@ -5,27 +5,35 @@
 #include <QMutex>
 #include <thread>
 #include <string>
+#include<vector>
+
 
 using namespace std;
 
+QMutex key1, key2;
 ifstream fin;
+
+
 
 
 
 class Thread : public QThread {
     private:
         int ID;
-        std::string target;
+        string target;
+        //string line;
 
     public:
-        Thread(int ID, string target, int startPos, int endPos){
+        Thread(int ID, string target){
             this -> ID = ID;
             this -> target = target;
+            // this -> line = line;
         }
 
         void run(){
+            cout << "Thread " + ID + " is running\n";
             string currentLine;
-
+            getline(fin, currentLine);
         }
 
 
@@ -37,20 +45,46 @@ class Thread : public QThread {
 int main(int argc, char** argv){
 
     int numberOfProcessors = (int) thread::hardware_concurrency();
+    int count = 0;
+    Thread* threads[numberOfProcessors];
 
-    cout << numberOfProcessors << std::endl;
 
-    // if (argc != 3){
-    //     std::cout << "Incorrect Number of Arguments!" <<std::endl;
-    //     return 0; //exit
-    // }
+
+    if (argc != 3){
+        std::cout << "Incorrect Number of Arguments!" <<std::endl;
+        return 0; //exit
+    }
 
     string target = argv[1];
     string fileName = argv[2];
 
-    // fin.open(fileName);
+    fin.open(fileName);
 
-    // if(fin.fail())
+    if(fin.fail()){
+        cout << "Failed to open file" << endl;
+        return 0;
+    }
+
+
+    while(!fin.eof()){
+        for(int i = 1; i <= numberOfProcessors; i++){
+            threads[i] = new Thread(i, target);
+            thread[i] -> start()
+
+            if(fin.eof()) // for the case where number of lines is not divisible by number of threads
+                break;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 
 
     return 0;
