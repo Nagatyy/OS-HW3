c6c4fdb42e8a9fde0589c55c4ad1a3d699db0f7e
Nagatyy <mohamednagaty@aucegypt.edu>
2020-10-13 00:26:48 +0200

    Commit6

--- program.cpp
+++ program.cpp
@@ -63,7 +63,7 @@ int main(int argc, char** argv){
 
     int numberOfProcessors = (int) thread::hardware_concurrency();
     Thread* threads[numberOfProcessors];
-    bool* isDone = new bool[numberOfProcessors];
+    bool* isDone[numberOfProcessors];
     // the above array is used to prevent threads leaking. A new thread will only be made if its
     // status in the above array is "true". It is set to true when a thread is finished executing
 
